926b9ac251e61142945640f33ac78bbcb168f19f
ydshinkk <yedingshin@gmail.com>
2022-12-25 18:46:36 +0900

    Forth - 221225 18:45 : DB 연동 실패 -> 날짜를 입력하게 만들고 insert에 들어갈 인자를 4개에서 2개로 줄일 예정

--- app/src/main/java/com/mobile/somute/MoodTracker.java
+++ app/src/main/java/com/mobile/somute/MoodTracker.java
@@ -6,14 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MoodTracker extends AppCompatActivity {
 
+    int cYear, cMonth, cDayOfMonth, cSoundNum;
     CalendarView calView;
     EditText et_date, et_soundNum;
+    Button btn_saveDB;
+    DBHelper dbHelper;
     ImageButton iBtn_mood1, iBtn_mood2, iBtn_mood3, iBtn_mood4, iBtn_mood5, iBtn_mood6, iBtn_mood7, iBtn_mood8;
     MediaPlayer mediaPlayer1, mediaPlayer2, mediaPlayer3, mediaPlayer4, mediaPlayer5, mediaPlayer6, mediaPlayer7, mediaPlayer8;
 
@@ -24,6 +29,8 @@ public class MoodTracker extends AppCompatActivity {
 
         calView = (CalendarView) findViewById(R.id.calView);
         et_date = (EditText) findViewById(R.id.et_date);
+        btn_saveDB = (Button) findViewById(R.id.btn_saveDB);
+        dbHelper =new DBHelper(MoodTracker.this, 1);
 
         iBtn_mood1 = (ImageButton) findViewById(R.id.iBtn_mood1);
         iBtn_mood2 = (ImageButton) findViewById(R.id.iBtn_mood2);
@@ -46,8 +53,10 @@ public class MoodTracker extends AppCompatActivity {
         calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                int pyear = year;
-                et_date.setText(year + "년 " + (month+1) + "월 " + dayOfMonth + "일");
+                cYear = year;
+                cMonth = month+1;
+                cDayOfMonth = dayOfMonth;
+                et_date.setText(year + "년 " + cMonth + "월 " + dayOfMonth + "일");
             }
         });
 
@@ -97,6 +106,37 @@ public class MoodTracker extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mediaPlayer8.start();
+            }
+        });
+
+        btn_saveDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(et_soundNum.getText().equals("1")){
+                    cSoundNum = 1;
+                }
+                else if(et_soundNum.getText().equals("2")){
+                    cSoundNum = 2;
+                }
+                else if(et_soundNum.getText().equals("3")){
+                    cSoundNum = 3;
+                }
+                else if(et_soundNum.getText().equals("4")){
+                    cSoundNum = 4;
+                }
+                else if(et_soundNum.getText().equals("5")){
+                    cSoundNum = 5;
+                }
+                else if(et_soundNum.getText().equals("6")){
+                    cSoundNum = 6;
+                }
+                else if(et_soundNum.getText().equals("7")){
+                    cSoundNum = 7;
+                }
+                else { cSoundNum = 8; }
+
+                dbHelper.insert(cYear, cMonth, cDayOfMonth, cSoundNum);
+                Toast.makeText(MoodTracker.this, "저장이 완료되었습니다!", Toast.LENGTH_LONG).show();
             }
         });
 
