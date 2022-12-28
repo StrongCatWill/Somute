e6b6ea4eb824b187e982a7bb60eb2448b15be921
StrongCatWill <ssyysskywalker@gamil.com>
2022-12-28 10:09:27 +0900

    //달력 위에 붙어야 하는데 지금 이슈는 * 1. 선택한 날짜 위로 붙을 수 있게 정확한 location값을 얻어내는 것이 어려움 * 2. 1~8까지의 무드만 미리 ImageView로 지정해두었으므로 한 달 안에서 하나의 mood가 중복으로 사용되는 경우는 처리가 불가능함.*/
    MoodTracker.javar과 activity_mood_tracker.xml 수정함. 구동은 하지 않음.
    위의 이슈를 제외하면 논리적으로는 문제 없음을 검증함.

--- app/src/main/java/com/mobile/somute/MoodTracker.java
+++ app/src/main/java/com/mobile/somute/MoodTracker.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.media.metrics.Event;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class MoodTracker extends AppCompatActivity {
 
@@ -63,6 +69,7 @@ public class MoodTracker extends AppCompatActivity {
             }
         });
 
+
         iBtn_mood1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -112,10 +119,75 @@ public class MoodTracker extends AppCompatActivity {
             }
         });
 
+
         btn_saveDB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbHelper.insert(cYear, cMonth, cDayOfMonth, Integer.parseInt(et_soundNum.getText().toString()));
+
+
+                //선택한 무드가 달력에 보이도록 하는 코드
+                if(Integer.parseInt(String.valueOf(et_soundNum))==1){
+                    ImageView iv_mood1_patch= findViewById(R.id.iv_mood1_patch);
+                    iv_mood1_patch.setVisibility(View.VISIBLE);
+                    iv_mood1_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood1_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else if(Integer.parseInt(String.valueOf(et_soundNum))==2){
+                    ImageView iv_mood2_patch= findViewById(R.id.iv_mood2_patch);
+                    iv_mood2_patch.setVisibility(View.VISIBLE);
+                    iv_mood2_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood2_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else if(Integer.parseInt(String.valueOf(et_soundNum))==3){
+                    ImageView iv_mood3_patch= findViewById(R.id.iv_mood3_patch);
+                    iv_mood3_patch.setVisibility(View.VISIBLE);
+                    iv_mood3_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood3_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else if(Integer.parseInt(String.valueOf(et_soundNum))==4){
+                    ImageView iv_mood4_patch= findViewById(R.id.iv_mood4_patch);
+                    iv_mood4_patch.setVisibility(View.VISIBLE);
+                    iv_mood4_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood4_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else if(Integer.parseInt(String.valueOf(et_soundNum))==5){
+                    ImageView iv_mood5_patch= findViewById(R.id.iv_mood5_patch);
+                    iv_mood5_patch.setVisibility(View.VISIBLE);
+                    iv_mood5_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood5_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else if(Integer.parseInt(String.valueOf(et_soundNum))==6){
+                    ImageView iv_mood6_patch= findViewById(R.id.iv_mood6_patch);
+                    iv_mood6_patch.setVisibility(View.VISIBLE);
+                    iv_mood6_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood6_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else if(Integer.parseInt(String.valueOf(et_soundNum))==7){
+                    ImageView iv_mood7_patch= findViewById(R.id.iv_mood7_patch);
+                    iv_mood7_patch.setVisibility(View.VISIBLE);
+                    iv_mood7_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood7_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                else{
+                    ImageView iv_mood8_patch= findViewById(R.id.iv_mood8_patch);
+                    iv_mood8_patch.setVisibility(View.VISIBLE);
+                    iv_mood8_patch.setX(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                    iv_mood8_patch.setY(Integer.parseInt(String.valueOf(cDayOfMonth)));
+                }
+
+                //달력 위에 붙어야 하는데 지금 이슈는
+                /*
+                * 1. 선택한 날짜 위로 붙을 수 있게 정확한 location값을 얻어내는 것이 어려움
+                * 2. 1~8까지의 무드만 미리 ImageView로 지정해두었으므로 한 달 안에서 하나의 mood가 중복으로 사용되는 경우는 처리가 불가능함.*/
+
                 Toast.makeText(MoodTracker.this, "저장이 완료되었습니다!", Toast.LENGTH_LONG).show();
             }
         });
